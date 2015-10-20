f0db2f545c6be1b31ebee410d0642d72c2caf84b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-10-20 21:27:46 +1100

    Add environment variable for enabling post mortem debugger.

--- 2.7/start.sh
+++ 2.7/start.sh
@@ -236,6 +236,10 @@ if test x"$MOD_WSGI_RELOAD_ON_CHANGES" != x""; then
     SERVER_ARGS="$SERVER_ARGS --reload-on-changes"
 fi
 
+if test x"$MOD_WSGI_ENABLE_DEBUGGER" != x""; then
+    SERVER_ARGS="$SERVER_ARGS --debug-mode --enable-debugger"
+fi
+
 if test x"$NEW_RELIC_LICENSE_KEY" != x"" -o \
         x"$NEW_RELIC_CONFIG_FILE" != x""; then
     SERVER_ARGS="$SERVER_ARGS --with-newrelic"
